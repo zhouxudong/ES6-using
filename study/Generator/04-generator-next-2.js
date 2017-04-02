69ba580f586cbab03cf7225a7dc83822c32c5f5f
zhouxudong <z_zxdong@163.com>
2017-04-02 23:31:25 +0800

    Generator next方法待参数

---
+++ study/Generator/04-generator-next-2.js
@@ -0,0 +1,14 @@
+function *foo(x) {
+    var y = 2 * (yield (x + 1));
+    var z = yield (y / 3);
+    return (x + y + z);
+}
+var a = foo(5);
+console.log(a.next()); // {value: 6, done: false}
+console.log(a.next()); // {value: NaN, done: false}
+console.log(a.next()); // {value: NaN, done: true}
+
+var b = foo(5);
+console.log(b.next());  // {value: 6, done: false}
+console.log(b.next(12));// {value: 8, done: false}
+console.log(b.next(13));// {value: 42, done: true}
