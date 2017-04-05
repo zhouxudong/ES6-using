e6ca36328c0d39475e922fd250f77216e027eb61
zhouxudong <z_zxdong@163.com>
2017-04-05 22:07:57 +0800

    fetch module

---
+++ study/Async/02-async-timeout.js
@@ -0,0 +1,11 @@
+function timeout(ms) {
+    return new Promise( resolve => {
+        setTimeout(resolve, ms);
+    })
+}
+
+async function asyncPrint(value, ms) {
+    await timeout(ms);
+    console.log(value);
+}
+asyncPrint("hello world", 1000);
